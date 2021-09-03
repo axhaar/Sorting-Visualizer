33eb34845ffa179ee52d8c6936471568cb6d2568
axhaar <pranshumishra2001@gmail.com>
2021-09-03 18:19:06 +0530

    generate array function implemented

--- js/main.js
+++ js/main.js
@@ -18,5 +18,11 @@ function generate_array()
 
     for(var i=0;i<array_size;i++)
     {
-        
+        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) + 10;
+        divs[i]=document.createElement("div");
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
     }
+}
+
