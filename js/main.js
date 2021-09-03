72957821c19d51f63dee219b7a5e99d42f4622ca
axhaar <pranshumishra2001@gmail.com>
2021-09-03 20:03:24 +0530

    merge sort implementation without colour

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function generate_array()
         divs[i]=document.createElement("div");
         cont.appendChild(divs[i]);
         margin_size=0.1;
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:aqua; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
     }
 }
 
