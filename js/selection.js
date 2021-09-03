be3de6b1f1b1227b7a8fa7cfe338cead44db347a
axhaar <pranshumishra2001@gmail.com>
2021-09-03 19:37:38 +0530

    fixed more bugs in selection.js

--- js/selection.js
+++ js/selection.js
@@ -1,21 +1,32 @@
 function Selection_sort()
 {
+    c_delay=0;
+
     for(var i=0;i<array_size-1;i++)
     {
-        if(div_sizes[j]<div_sizes[index_min])
+        div_update(divs[i],div_sizes[i],"red");
+
+        index_min=i;
+
+        for(var j=i+1;j<array_size;j++)
         {
-            if(index_min!=i)
+            div_update(divs[j],div_sizes[j],"yellow");
+
+            if(div_sizes[j]<div_sizes[index_min])
+            {
+                if(index_min!=i)
                 {
                     div_update(divs[index_min],div_sizes[index_min],"blue");
                 }
                 index_min=j;
                 div_update(divs[index_min],div_sizes[index_min],"red");
-        }   
-        else
-        {
+            }
+            else
+            {
                 div_update(divs[j],div_sizes[j],"blue");
+            }
         }
-
+        
         if(index_min!=i)
         {
             var temp=div_sizes[index_min];
