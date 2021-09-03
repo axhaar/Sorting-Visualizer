1ac04dbae2610baf6903724ce0cf880ec71a6b6d
axhaar <pranshumishra2001@gmail.com>
2021-09-03 18:55:20 +0530

    color test2

--- js/bubble.js
+++ js/bubble.js
@@ -20,6 +20,9 @@ function Bubble()
             }
             div_update(divs[j],div_sizes[j], "blue");
         }
-        
+        div_update(divs[j],div_sizes[j], "green");
     }
+    div_update(divs[0],div_sizes[0], "green");
+
+    
 }
