ebecb936ed550cbbb7d365ef5420367e3c598ba5
axhaar <pranshumishra2001@gmail.com>
2021-09-03 19:04:17 +0530

    implemented vis_speed()

--- js/visuals.js
+++ js/visuals.js
@@ -1,3 +1,21 @@
 var speed=1000;
 
 inp_aspeed.addEventListener("input",vis_speed);
+
+function vis_speed()
+{
+    var array_speed=inp_aspeed.value;
+    switch(parseInt(array_speed))
+    {
+        case 1: speed=1;
+                break;
+        case 2: speed=10;
+                break;
+        case 3: speed=100;
+                break;
+        case 4: speed=1000;
+                break;
+        case 5: speed=10000;
+                break;
+    }
+}
