ff9d75831e4991e5d34e9801c913317ed07dccc4
axhaar <pranshumishra2001@gmail.com>
2021-09-03 18:48:33 +0530

    basic algo for bubble sort

--- js/bubble.js
+++ js/bubble.js
@@ -4,6 +4,12 @@ function Bubble()
     {
         for(var j=0;j<array_size-i-1;j++)
         {
-            
+            div_update(divs[j],div_sizes[j],"yellow");
+            if(div_sizes[j]>div_sizes[j+1])
+            {
+                var temp=div_sizes[j];
+                div_sizes[j]=div_sizes[j+1];
+                div_sizes[j+1]=temp;
+            }
         }
 }
