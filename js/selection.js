c320359fb65b755aaaf6d4183cbac0fc7b2c7d19
axhaar <pranshumishra2001@gmail.com>
2021-09-03 19:29:25 +0530

    selection in progress

--- js/selection.js
+++ js/selection.js
@@ -2,6 +2,17 @@ function Selection_sort()
 {
     for(var i=0;i<array_size-1;i++)
     {
-        
+        if(div_sizes[j]<div_sizes[index_min])
+        {
+            if(index_min!=i)
+                {
+                    
+                }
+                index_min=j;
+            else
+            {
+                
+            }
+        }
     }
 }
