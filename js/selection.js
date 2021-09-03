2c974785422ffb85e65843edbeb1056bed810054
axhaar <pranshumishra2001@gmail.com>
2021-09-03 19:30:58 +0530

    selection2

--- js/selection.js
+++ js/selection.js
@@ -6,13 +6,21 @@ function Selection_sort()
         {
             if(index_min!=i)
                 {
-                    
+                    div_update(divs[index_min],div_sizes[index_min],"blue");
                 }
                 index_min=j;
+                div_update(divs[index_min],div_sizes[index_min],"red");
             else
             {
-                
+                div_update(divs[j],div_sizes[j],"blue");
             }
+        }
+
+        if(index_min!=i)
+        {
+            var temp=div_sizes[index_min];
+            div_sizes[index_min]=div_sizes[i];
+            div_sizes[i]=temp;
         }
     }
 }
